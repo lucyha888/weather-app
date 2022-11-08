66a8dfe9e5723e7d4081129f53f3877cf2f8b9e1
lucyha <lucyha.0101@gmail.com>
2022-11-08 22:21:10 +0700

    Create Weather App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+
+const BASE_URL = "https://openweathermap.org/data/2.5/find"
+const API_KEY = "439d4b804bc8187953eb36d2a8c26a02"
 
 function App() {
+  const [value, setValue] = useState("")
+  const [result, setResult] = useState("")
+  const [description, setDescription] = useState("")
+  const [isCall, setIsCall] = useState(false)
+  useEffect(() => {
+    if (!value) return;
+    const fetchTemperature = async () => {
+      const response = await fetch(`${BASE_URL}?q=${value}&appid=${API_KEY}`)
+      const data = await response.json()
+      setResult(data.list[0]?.main?.temp)
+      setDescription(data.list[0]?.weather[0]?.description)
+    }
+    fetchTemperature()
+    setIsCall(false)
+    // eslint-disable-next-line 
+  }, [isCall])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Weather app</h2>
+      <input type="text" placeholder='Please input the city' value={value} onChange={e => setValue(e.target.value)} />
+      <button onClick={() => setIsCall(true)}>Search</button>
+      <p>Temp of {value}: {result}, description: {description}</p>
     </div>
   );
 }
