d6ccc04b400d120eb9a654ef0c9ad5814ab15574
lucyha <lucyha.0101@gmail.com>
2022-11-09 23:03:20 +0700

    Fix Bug

--- src/App.js
+++ src/App.js
@@ -9,25 +9,44 @@ function App() {
   const [result, setResult] = useState("")
   const [description, setDescription] = useState("")
   const [isCall, setIsCall] = useState(false)
+  const [error, setError] = useState("")
   useEffect(() => {
     if (!value) return;
     const fetchTemperature = async () => {
-      const response = await fetch(`${BASE_URL}?q=${value}&appid=${API_KEY}`)
-      const data = await response.json()
-      setResult(data.list[0]?.main?.temp)
-      setDescription(data.list[0]?.weather[0]?.description)
+      try {
+        const response = await fetch(`${BASE_URL}?q=${value}&appid=${API_KEY}`)
+        const data = await response.json()
+        setResult(data.list[0]?.main?.temp)
+        setDescription(data.list[0]?.weather[0]?.description)
+        if (data.list.length === 0) {
+          setError("Data Not Found")
+        } else {
+          setError("")
+        }
+      } catch(error){
+        console.log(error)
+      }
+    
     }
     fetchTemperature()
     setIsCall(false)
     // eslint-disable-next-line 
   }, [isCall])
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setIsCall(true)
+  }
   return (
     <div className="App">
       <h2>Weather app</h2>
-      <input type="text" placeholder='Please input the city' value={value} onChange={e => setValue(e.target.value)} />
-      <button onClick={() => setIsCall(true)}>Search</button>
-      <p>Temperature of {value}: {result}, description: {description}</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Please input the city' value={value} onChange={e => setValue(e.target.value)} />
+        <button type="submit">Search</button>
+      </form>
+      {error ?<p style={{color:"red"}}>{error}</p> :<p>Temperature of {value}: {result}, description: {description}</p> }
+      
     </div>
+    
   );
 }
 
